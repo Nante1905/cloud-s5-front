5e2a562fc46db5e6b0b7b31309c22e97c23b76c9
Mialy05 <mialisoamurielle@gmail.com>
2024-01-18 09:28:55 +0300

    [init] toutes les dépendance, font et react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { frFR } from "@mui/x-date-pickers";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import LandingPage from "./components/landing-page/pages/landing-page.component.tsx";
+
+const theme = createTheme(
+  {
+    typography: {
+      fontFamily: "Jost",
+    },
+  },
+  frFR
+);
+
+const routes = createBrowserRouter([
+  {
+    path: "",
+    element: (
+      <App>
+        <Outlet />
+      </App>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={routes} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
