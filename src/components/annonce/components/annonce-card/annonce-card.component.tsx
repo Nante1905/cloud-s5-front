2f4a724a892403d301542caf1f72225625a3f156
Mialy05 <mialisoamurielle@gmail.com>
2024-01-21 18:46:45 +0300

    feat [LISTE ANNONCE] ajout etat

--- src/components/annonce/components/annonce-card/annonce-card.component.tsx
+++ src/components/annonce/components/annonce-card/annonce-card.component.tsx
@@ -1,6 +1,6 @@
 import Favorite from "@mui/icons-material/Favorite";
 import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
-import { Card, Checkbox } from "@mui/material";
+import { Badge, Card, Checkbox } from "@mui/material";
 import dayjs from "dayjs";
 import { useCallback } from "react";
 import { Carousel } from "react-responsive-carousel";
@@ -38,45 +38,62 @@ const AnnonceCard = (props: AnnonceCardProps) => {
     }
   }, []);
 
+  const renderClassName = useCallback((note: number) => {
+    if (note >= 7) {
+      return "success";
+    } else if (note >= 4) {
+      return "warning";
+    }
+    return "danger";
+  }, []);
+
   return (
     <div>
-      <Card className="annonce-card">
-        <div className="annonce-images">
-          {/* <img src="/images/logo-fit.png" alt="" /> */}
-          <Carousel
-            showArrows={false}
-            axis="horizontal"
-            emulateTouch={true}
-            statusFormatter={(current: number, total: number) =>
-              `${current}/${total}`
-            }
-          >
-            {annonce.photos.map((p, index) => (
-              <div key={`img_${index}`}>
-                <img src={p.url} />
-              </div>
-            ))}
-          </Carousel>
-        </div>
-        <div className="annonce-info">
-          <h2 className="annonce-title light no-margin">
-            {annonce.voiture.modele.marque.nom}: {annonce.voiture.modele.nom}
-          </h2>
-          <div className="utilisateur">
-            <p className="no-margin">
-              {annonce.utilisateur.nom} {annonce.utilisateur.prenom}
-            </p>
-            <small>{parseDate(annonce.dateCreation)}</small>
+      <Badge
+        badgeContent={`${annonce.voiture.etat}/10`}
+        className={renderClassName(annonce.voiture.etat)}
+      >
+        <Card className="annonce-card">
+          <div className="annonce-images">
+            {/* <img src="/images/logo-fit.png" alt="" /> */}
+            <Carousel
+              showArrows={false}
+              axis="horizontal"
+              emulateTouch={true}
+              statusFormatter={(current: number, total: number) =>
+                `${current}/${total}`
+              }
+            >
+              {annonce.photos.map((p, index) => (
+                <div key={`img_${index}`}>
+                  <img src={p.url} />
+                </div>
+              ))}
+            </Carousel>
           </div>
-          <h3 className="no-margin">{annonce.prix.toLocaleString("fr")} MGA</h3>
-        </div>
-        <div className="favorite-icon">
-          <Checkbox
-            icon={<FavoriteBorder fontSize="large" />}
-            checkedIcon={<Favorite fontSize="large" />}
-          />
-        </div>
-      </Card>
+          <div className="annonce-info">
+            <h2 className="annonce-title light no-margin">
+              {annonce.voiture.modele.marque.nom}: {annonce.voiture.modele.nom}
+            </h2>
+            <h2 className="light no-margin">Etat: {annonce.voiture.etat}/10</h2>
+            <div className="utilisateur">
+              <p className="no-margin">
+                {annonce.utilisateur.nom} {annonce.utilisateur.prenom}
+              </p>
+              <small>{parseDate(annonce.dateCreation)}</small>
+            </div>
+            <h3 className="no-margin">
+              {annonce.prix.toLocaleString("fr")} MGA
+            </h3>
+          </div>
+          <div className="favorite-icon">
+            <Checkbox
+              icon={<FavoriteBorder fontSize="large" />}
+              checkedIcon={<Favorite fontSize="large" />}
+            />
+          </div>
+        </Card>
+      </Badge>
     </div>
   );
 };
