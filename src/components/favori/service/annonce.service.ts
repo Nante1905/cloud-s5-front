cfe2ed5e74318b539fc519c8c4ad7b57d40a5065
Mialy05 <mialisoamurielle@gmail.com>
2024-01-24 22:59:48 +0300

    feat [INTERCEPTOR] ajout intercepteur pour ajouter le token dans les reqûetes si nécessaire

--- src/components/favori/service/annonce.service.ts
+++ src/components/favori/service/annonce.service.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { URL_API } from "../../../shared/env/env";
 import { TAILLE_PAGE } from "../../../shared/constants/constants";
+import { http } from "../../../shared/services/interceptor/axios.interceptor";
 
 
-export const findAllFavori = (page: number) => axios.get(`${URL_API}/favoris?page=${page}&taille=${TAILLE_PAGE}`)
+export const findAllFavori = (page: number) => http.get(`${URL_API}/favoris?page=${page}&taille=${TAILLE_PAGE}`)
