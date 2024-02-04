bf232c8f0b06e8ddc9baffbfac36dccf8dba00e4
Mialy05 <mialisoamurielle@gmail.com>
2024-02-04 15:25:57 +0300

    feat [DOWNLOAD PAGE] page téléchargement app mobile

---
+++ src/components/download-page/pages/download-page.root.tsx
@@ -0,0 +1,41 @@
+import QRCode from "react-qr-code";
+import { MOBILE_APP } from "../../../shared/env/env";
+import "./download-page.root.scss";
+
+const DownloadPage = () => {
+  return (
+    <div className="download-bg">
+      <div className="landing-text">
+        <div className="content">
+          <div className="text">
+            <h1 className="landing-title primary-text">
+              {" "}
+              <div></div> Installez notre app mobile
+            </h1>
+            <p>
+              Faites partie de la communauté <strong>Sera Vaika</strong> en
+              téléchargeant l'application mobile et en vous inscrivant. Ainsi,
+              vous pourriez mettre votre voiture en vente, créer de nouvelles
+              annonces et vous aurez une vue d'ensemble sur toutes vos annonces.
+              N'attendez plus, télécharger l'application en scannant ce QR Code,
+              et inscrivez-vous en seulement quelques clics.
+            </p>
+          </div>
+          <div className="div-action">
+            <div className="div_qr-code">
+              <QRCode value={MOBILE_APP} fgColor="#000" bgColor="transparent" />
+            </div>
+            <div className="logo">
+              <img src="/images/logo-transparent.png" alt="logo" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="img-container">
+        <img src="/images/phone-v2.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default DownloadPage;
