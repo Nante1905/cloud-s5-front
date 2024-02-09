b1e7959edc982920391fe79d2e5d3c3e933d3623
Mialy05 <mialisoamurielle@gmail.com>
2024-02-09 22:12:03 +0300

    fix css image and modify env

--- src/shared/env/env.ts
+++ src/shared/env/env.ts
@@ -1,7 +1,6 @@
 // TODO: verifiena daholo ny url
 export const URL_API = "https://cloud-s5-back-production-1b1c.up.railway.app";
-// export const URL_API = "https://cloud-s5-back-production.up.railway.app";
 // export const URL_API = "http://localhost:8080";
-export const MOBILE_APP = "TODO: ovaina url an'ilay apk"
-export const SOCKET_URL = "http://localhost:9092";
-// export const SOCKET_URL = "https://cloud-s5-back-production-1b1c.up.railway.app";
+// TODO: ovaina url an'ilay app
+export const MOBILE_APP = "https://drive.google.com/file/d/1zSAPOGtTZDJDGfnNd-0fW5dnkAXeg5Q9/view?usp=drive_link"
+export const SOCKET_URL = "https://cloud-s5-back-production-1b1c.up.railway.app";
